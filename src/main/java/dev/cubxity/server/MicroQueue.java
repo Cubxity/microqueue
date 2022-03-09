7a3024c5fb29fd53c3227a389b5a2f2611933cee
Cubxity <contact@cubxity.dev>
2022-03-09 17:18:09 +0100

    feat: configuration support

--- src/main/java/dev/cubxity/server/MicroQueue.java
+++ src/main/java/dev/cubxity/server/MicroQueue.java
@@ -5,18 +5,35 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.PlayerLoginEvent;
-import net.minestom.server.event.player.PlayerMoveEvent;
-import net.minestom.server.event.player.PlayerSpawnEvent;
+import net.minestom.server.event.player.*;
+import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.potion.Potion;
 import net.minestom.server.potion.PotionEffect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class MicroQueue {
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        Logger logger = LoggerFactory.getLogger(MicroQueue.class);
+
+        logger.info("Starting MicroQueue.");
+
+        // Configuration
+        MicroQueueConfig config = new MicroQueueConfig();
+        config.saveDefaults();
+        config.load();
+
+        System.setProperty("minestom.chunk-view-distance", String.valueOf(config.getViewDistance()));
+
+        // Initialization
         MinecraftServer server = MinecraftServer.init();
+        MinecraftServer.setBrandName(config.getBrandName());
+        MinecraftServer.setCompressionThreshold(config.getCompressionThreshold());
+
         GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
 
@@ -27,10 +44,27 @@ public final class MicroQueue {
         instanceContainer.setChunkGenerator(new VoidChunkGenerator());
         instanceContainer.enableAutoChunkLoad(true);
 
-        instanceContainer.setBlock(0, 62, 0, Block.BEDROCK);
+        /* Y=62 */
+        instanceContainer.setBlock(0, 62, 0, Block.BARRIER);
+
+        /* Y=63 */
+        instanceContainer.setBlock(1, 63, 0, Block.BARRIER);
+        instanceContainer.setBlock(-1, 63, 0, Block.BARRIER);
+        instanceContainer.setBlock(0, 63, 1, Block.BARRIER);
+        instanceContainer.setBlock(0, 63, -1, Block.BARRIER);
+
+        /* Y=64 */
+        instanceContainer.setBlock(1, 64, 0, Block.BARRIER);
+        instanceContainer.setBlock(-1, 64, 0, Block.BARRIER);
+        instanceContainer.setBlock(0, 64, 1, Block.BARRIER);
+        instanceContainer.setBlock(0, 64, -1, Block.BARRIER);
+
+        /* Y=65 */
+        instanceContainer.setBlock(0, 65, 0, Block.BARRIER);
 
         Pos spawnPoint = new Pos(0.5, 63.0, 0.5);
 
+        // Events
         eventHandler.addListener(PlayerLoginEvent.class, event -> {
             Player player = event.getPlayer();
             player.setFlying(true);
@@ -43,20 +77,38 @@ public final class MicroQueue {
         eventHandler.addListener(PlayerSpawnEvent.class, event -> {
             Player player = event.getPlayer();
 
-            player.setGameMode(GameMode.SPECTATOR);
+            player.setGameMode(GameMode.ADVENTURE);
             player.setEnableRespawnScreen(false);
 
-            player.setGravity(0.0, 0.0);
             player.setNoGravity(true);
 
             player.addEffect(new Potion(PotionEffect.BLINDNESS, Byte.MAX_VALUE, Integer.MAX_VALUE));
             player.addEffect(new Potion(PotionEffect.INVISIBILITY, Byte.MAX_VALUE, Integer.MAX_VALUE));
+            player.addEffect(new Potion(PotionEffect.SLOWNESS, Byte.MAX_VALUE, Integer.MAX_VALUE));
         });
 
         eventHandler.addListener(PlayerMoveEvent.class, event -> {
+            if (!event.getNewPosition().sameBlock(event.getPlayer().getPosition())) {
+                event.setCancelled(true);
+            }
+        });
+
+        eventHandler.addListener(PlayerChatEvent.class, event -> {
             event.setCancelled(true);
         });
 
-        server.start("0.0.0.0", 25565);
+        // Velocity support
+        String velocitySecret = System.getProperty("VELOCITY_SECRET");
+        if (velocitySecret != null) {
+            logger.info("Velocity supported enabled.");
+            VelocityProxy.enable(velocitySecret);
+        }
+
+        // Start
+        server.start("0.0.0.0", config.getServerPort());
+
+        // Start complete
+        double timeElapsed = (System.nanoTime() - startTime) / 1.0E6;
+        logger.info("Done (" + timeElapsed + " ms)! For help, type nothing.");
     }
 }
---
+++ src/main/java/dev/cubxity/server/MicroQueueConfig.java
@@ -0,0 +1,92 @@
+package dev.cubxity.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class MicroQueueConfig {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Properties properties = new Properties();
+
+    private String brandName = "microqueue";
+    private int serverPort = 25565;
+    private int compressionThreshold = 0; // 0: Disable
+    private int viewDistance = 2;
+    private int maxPlayers = 0; // 0: Disable
+
+    public void saveDefaults() {
+        try {
+            File file = new File("server.properties");
+            if (!file.exists()) {
+                try (FileOutputStream out = new FileOutputStream("server.properties")) {
+                    try (InputStream in = getClass().getResourceAsStream("/server.properties")) {
+                        byte[] buf = new byte[8192];
+                        int length;
+                        while ((length = in.read(buf)) > 0) {
+                            out.write(buf, 0, length);
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            logger.error("Unable to save default configuration", ex);
+        }
+    }
+
+    public void load() {
+        try (FileInputStream stream = new FileInputStream("server.properties")) {
+            properties.load(stream);
+        } catch (Exception ex) {
+            logger.error("Unable to load configuration", ex);
+        }
+
+        try {
+            brandName = properties.getProperty("brand-name", "microqueue");
+            serverPort = getInt("server-port", 25565);
+            compressionThreshold = Math.max(getInt("compression-threshold", 0), 0);
+            viewDistance = Math.max(getInt("view-distance", 2), 2);
+            maxPlayers = Math.max(getInt("max-players", 0), 0);
+        } catch (Exception ex) {
+            logger.error("Unable to parse configuration", ex);
+        }
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public int getCompressionThreshold() {
+        return compressionThreshold;
+    }
+
+    public int getViewDistance() {
+        return viewDistance;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    private int getInt(String key, int def) {
+        try {
+            String value = properties.getProperty(key);
+            if (value != null) {
+                return Integer.parseInt(value);
+            }
+        } catch (NumberFormatException ex) {
+            logger.warn("Invalid configuration value for key '" + key + "'", ex);
+        }
+
+        return def;
+    }
+
+}
